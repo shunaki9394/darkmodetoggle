7fd23e5cf62c91733220d4f09650c5b4df75efee
Yeong Wei <tanyeongwei@qbix.tech>
2025-08-21 16:36:34 +0800

    init update

---
+++ hook.php
@@ -0,0 +1,5 @@
+
+<?php
+if (!defined('GLPI_ROOT')) { die('Direct access not allowed'); }
+function plugin_thememode_install() { return true; }
+function plugin_thememode_uninstall() { return true; }
---
+++ setup.php
@@ -0,0 +1,24 @@
+
+<?php
+if (!defined('GLPI_ROOT')) { die('Direct access not allowed'); }
+
+function plugin_init_thememode() {
+   global $PLUGIN_HOOKS;
+   $PLUGIN_HOOKS['add_css']['thememode'] = ['css/dark.css', 'css/toggle.css'];
+   $PLUGIN_HOOKS['add_javascript']['thememode'] = ['js/thememode.js'];
+   $PLUGIN_HOOKS['csrf_compliant']['thememode'] = true;
+}
+
+function plugin_version_thememode() {
+   return [
+      'name'           => 'Theme Mode (Dark/Light Toggle)',
+      'version'        => '2.2.0',
+      'author'         => 'Qbix / ChatGPT',
+      'license'        => 'MIT',
+      'homepage'       => 'https://qbix.tech',
+      'minGlpiVersion' => '10.0.0'
+   ];
+}
+
+function plugin_thememode_check_prerequisites() { return version_compare(GLPI_VERSION, '10.0.0', '>='); }
+function plugin_thememode_check_config($verbose = false) { return true; }
